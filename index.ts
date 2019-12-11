fe600f7d583b416dc8884e19abcd5fb1fe71d3b4
Benjamin Hogl <benjamin.hogl@jambit.com>
2019-12-11 19:00:58 +0100

    Improve index.js

--- index.ts
+++ index.ts
@@ -1,21 +1,43 @@
 import { formatFilename } from './utils';
 
-async function printSolutions(year = 2019) {
-    for (let day = 25; day >= 1; day--) {
-        try {
-            let { solution1, solution2 } = await import(formatFilename(day, year))
-            const outputs = [`Day ${day}`]
-            if (typeof solution1 === "function") {
-                outputs.push(`Solution 1: ${solution1()}`)
-            }
-            if (typeof solution2 === "function") {
-                outputs.push(`Solution 2: ${solution2()}`)
-            }
-            console.log(outputs.join(" - "))
-        } catch (error) {
-            // ignore import errors
+const defaultYear = 2019;
+
+async function printSolution(day: number, year = defaultYear) {
+    try {
+        let { solution1, solution2 } = await import(formatFilename(day, year))
+        const outputs = [`Day ${day}`]
+        if (typeof solution1 === "function") {
+            outputs.push(`Solution 1: ${solution1()}`)
+        }
+        if (typeof solution2 === "function") {
+            outputs.push(`Solution 2: ${solution2()}`)
+        }
+        console.log(outputs.join(" - "))
+    } catch (error) {
+        // ignore import errors, but not other errors
+        if (error.code !== "MODULE_NOT_FOUND") {
+            throw error;
         }
     }
 }
+async function printAllSolutions(year = defaultYear) {
+    console.log(`Printing all solutions of ${year}:`);
+    for (let day = 1; day <= 25; day++) {
+        printSolution(day, year)
+    }
+}
 
-printSolutions()
+const args = process.argv.slice(2);
+if (args.length > 0) {
+    const day = parseInt(args[0], 10);
+    const year = parseInt(args[1], 10) || defaultYear;
+    if (day < 100) {
+        console.log(`Printing solutions for day ${day} of ${year}:`);
+        printSolution(day, year);
+    } else {
+        const year = day; // year as only input
+        printAllSolutions(year);
+    }
+} else {
+    printAllSolutions();
+}
