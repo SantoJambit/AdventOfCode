0cadee3209e19896e62b71fade1e2e6276e8cad7
Benjamin Hogl <benjamin.hogl@jambit.com>
2020-12-04 23:41:15 +0100

    Autoformat common files, normalizeNewlines

--- index.ts
+++ index.ts
@@ -1,43 +1,43 @@
-import { formatFilename } from './utils';
+import { formatFilename } from "./utils";
 
-const defaultYear = 2019;
+const defaultYear = new Date().getFullYear();
 
 async function printSolution(day: number, year = defaultYear) {
-    try {
-        let { solution1, solution2 } = await import(formatFilename(day, year))
-        const outputs = [`Day ${day}`]
-        if (typeof solution1 === "function") {
-            outputs.push(`Solution 1: ${solution1()}`)
-        }
-        if (typeof solution2 === "function") {
-            outputs.push(`Solution 2: ${solution2()}`)
-        }
-        console.log(outputs.join(" - "))
-    } catch (error) {
-        // ignore import errors, but not other errors
-        if (error.code !== "MODULE_NOT_FOUND") {
-            throw error;
-        }
+  try {
+    let { solution1, solution2 } = await import(formatFilename(day, year));
+    const outputs = [`Day ${day}`];
+    if (typeof solution1 === "function") {
+      outputs.push(`Solution 1: ${solution1()}`);
     }
+    if (typeof solution2 === "function") {
+      outputs.push(`Solution 2: ${solution2()}`);
+    }
+    console.log(outputs.join(" - "));
+  } catch (error) {
+    // ignore import errors, but not other errors
+    if (error.code !== "MODULE_NOT_FOUND") {
+      throw error;
+    }
+  }
 }
 async function printAllSolutions(year = defaultYear) {
-    console.log(`Printing all solutions of ${year}:`);
-    for (let day = 1; day <= 25; day++) {
-        printSolution(day, year)
-    }
+  console.log(`Printing all solutions of ${year}:`);
+  for (let day = 1; day <= 25; day++) {
+    printSolution(day, year);
+  }
 }
 
 const args = process.argv.slice(2);
 if (args.length > 0) {
-    const day = parseInt(args[0], 10);
-    const year = parseInt(args[1], 10) || defaultYear;
-    if (day < 100) {
-        console.log(`Printing solutions for day ${day} of ${year}:`);
-        printSolution(day, year);
-    } else {
-        const year = day; // year as only input
-        printAllSolutions(year);
-    }
+  const day = parseInt(args[0], 10);
+  const year = parseInt(args[1], 10) || defaultYear;
+  if (day < 100) {
+    console.log(`Printing solutions for day ${day} of ${year}:`);
+    printSolution(day, year);
+  } else {
+    const year = day; // year as only input
+    printAllSolutions(year);
+  }
 } else {
-    printAllSolutions();
+  printAllSolutions();
 }
--- utils.ts
+++ utils.ts
@@ -1,17 +1,24 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
-const defaultYear = 2019;
-export function formatFilename(day: number, year: number, suffix = '') {
-    return `./${year}/${String(day).padStart(2, '0')}${suffix}`;
+const defaultYear = 2019; // for legacy reasons
+
+const normalizeNewlines = (str: string) => str.replace(/\r\n/g, "\n");
+
+export function formatFilename(day: number, year: number, suffix = "") {
+  return `./${year}/${String(day).padStart(2, "0")}${suffix}`;
 }
 export function getInput(day: number, year = defaultYear) {
-    return readFileSync(formatFilename(day, year, '.input.txt'), 'utf8');
+  return normalizeNewlines(
+    readFileSync(formatFilename(day, year, ".input.txt"), "utf8")
+  );
 }
 export function getInputArray({ day, year = defaultYear, separator }) {
-    const text = getInput(day, year);
-    const arr = text.split(separator);
-    return arr.filter(s => s !== ''); //filters out empty elements
+  const text = getInput(day, year);
+  const arr = text.split(separator);
+  return arr.filter((s: string) => s !== ""); //filters out empty elements
 }
 export function getProgram({ day, year = defaultYear }) {
-    return getInputArray({ day, year, separator: ',' }).map(s => parseInt(s, 10));
+  return getInputArray({ day, year, separator: "," }).map((s: string) =>
+    parseInt(s, 10)
+  );
 }
